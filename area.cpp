30187309bcbcf285dd679cfa63a4daa0675e6e3a
ChrisG <78868329+Geezman2006@users.noreply.github.com>
2021-02-11 22:40:23 -0500

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,23 @@
+#include  <iostream>
+using namespace std;
+
+int main()
+{
+		int length;
+		int width;
+		int area;
+		
+		area = length * width;
+		
+		cout << "Enter the Length ";
+		cin >> length;
+		
+		cout << "Enter the Width ";
+		cin >> width;
+		
+		cout <<"The area is ";
+		cout << length * width ;
+		
+		
+		return 0;
+}
